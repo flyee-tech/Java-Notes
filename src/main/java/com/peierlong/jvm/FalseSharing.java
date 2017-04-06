934844f47e2c315f035b7d267f29fee9c42a0975
peierlong <peielcn@gmail.com>
2017-04-06 18:53:27 +0800

    伪共享（False Sharing）对性能的影响

---
+++ src/main/java/com/peierlong/jvm/FalseSharing.java
@@ -0,0 +1,71 @@
+package com.peierlong.jvm;
+
+/**
+ * 包名: com.peierlong.jvm
+ * 创建人 : Elong
+ * 时间: 06/04/2017 6:38 PM
+ * 描述 : 根据运行结果，可以看到伪共享（False Sharing）对性能的影响
+ */
+public final class FalseSharing implements Runnable {
+
+    public final static int NUM_THREADS = 4; // change
+    public final static long ITERATIONS = 500L * 1000L * 1000L;
+    private final int arrayIndex;
+
+    private static VolatileLong[] longs = new VolatileLong[NUM_THREADS];
+
+    static {
+        for (int i = 0; i < longs.length; i++) {
+            longs[i] = new VolatileLong();
+        }
+    }
+
+    public FalseSharing(final int arrayIndex) {
+        this.arrayIndex = arrayIndex;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final long start = System.nanoTime();
+        runTest();
+        System.out.println("duration = " + (System.nanoTime() - start));
+    }
+
+    private static void runTest() throws InterruptedException {
+        Thread[] threads = new Thread[NUM_THREADS];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new FalseSharing(i));
+        }
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+    }
+
+    public void run() {
+        long i = ITERATIONS + 1;
+        while (0 != --i) {
+            longs[arrayIndex].value = i;
+        }
+    }
+
+    public final static class VolatileLong {
+        public volatile long value = 0L;
+        public long p1, p2, p3, p4, p5, p6; // comment out
+    }
+
+    /*
+     运行结果:
+     线程数       填充           不填充
+     4           4850679		44142519000
+     3           4317808		34061760
+     2           4132944		19784708
+     1           4371287		4283115
+     */
+
+
+}
