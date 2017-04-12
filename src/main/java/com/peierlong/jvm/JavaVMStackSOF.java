8d42c34716c9b85090be5b8ce0bed383a004ac04
peierlong <peielcn@gmail.com>
2017-04-12 18:09:04 +0800

    虚拟机栈和本地方法栈OOM/SOF测试

--- src/main/java/com/peierlong/jvm/JavaVMStackSOF.java
+++ src/main/java/com/peierlong/jvm/JavaVMStackSOF.java
@@ -5,6 +5,7 @@ package com.peierlong.jvm;
  * 创建人 : Elong
  * 时间: 12/04/2017 4:30 PM
  * 描述 : 虚拟机栈和本地方法栈OOM/SOF测试
+ * JVM参数 : -Xss160k
  */
 public class JavaVMStackSOF {
     private int stackLength = 0;
