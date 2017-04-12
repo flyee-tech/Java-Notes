b1935736d87b518a1f25416c87d53519dca96e09
peierlong <peielcn@gmail.com>
2017-04-12 18:08:40 +0800

    虚拟机栈和本地方法栈OOM/SOF测试

---
+++ src/main/java/com/peierlong/jvm/JavaVMStackSOF.java
@@ -0,0 +1,31 @@
+package com.peierlong.jvm;
+
+/**
+ * 包名: com.peierlong.jvm
+ * 创建人 : Elong
+ * 时间: 12/04/2017 4:30 PM
+ * 描述 : 虚拟机栈和本地方法栈OOM/SOF测试
+ */
+public class JavaVMStackSOF {
+    private int stackLength = 0;
+
+    private void stackLeak() {
+        stackLength ++;
+        stackLeak();
+    }
+
+
+    public static void main(String[] args) {
+        JavaVMStackSOF javaVMStackSOF = new JavaVMStackSOF();
+
+        try {
+            javaVMStackSOF.stackLeak();
+        } catch (Exception e) {
+            System.out.println(javaVMStackSOF.stackLength);
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
