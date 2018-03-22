40b5e5d64845dcea62ab115d49f0c063c3bfc59c
peierlong <peielcn@gmail.com>
2018-03-22 13:52:23 +0800

    了解class文件结构使用的测试类

---
+++ src/main/java/com/peierlong/clazz/TestClass.java
@@ -0,0 +1,16 @@
+package com.peierlong.clazz;
+
+/**
+ * @author elong
+ * @version V1.0
+ * @date 2018/3/22
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+
+}
