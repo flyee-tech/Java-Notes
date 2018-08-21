7bc90d04571393968b65c230eabfba9eaa4b923a
peierlong <peielcn@gmail.com>
2018-08-21 14:30:02 +0800

    condition await() signalAll() demo

---
+++ src/main/java/com/peierlong/concurrency/ConditionTest.java
@@ -0,0 +1,52 @@
+package com.peierlong.concurrency;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author elong
+ * @version V1.0
+ * @date 2018/8/21
+ */
+public class ConditionTest {
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public void before() {
+        lock.lock();
+        try {
+            System.out.println("before");
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void after() {
+        lock.lock();
+        try {
+            condition.await();
+            System.out.println("after");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        ConditionTest conditionTest = new ConditionTest();
+
+        executorService.execute(conditionTest::after);
+        executorService.execute(conditionTest::before);
+
+
+    }
+
+
+}
